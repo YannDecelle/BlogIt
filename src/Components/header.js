67601111aa2b001cd8b727320c4aeedf3ea11988
TAHATJ48 <thetotoj@gmail.com>
2024-06-06 15:27:43 +0200

    auth link

--- src/Components/header.js
+++ src/Components/header.js
@@ -14,7 +14,7 @@ const Header = () => {
                 <Link color="inherit" underline="none" href="/" style={{ marginRight: '16px' }}>Home</Link>
                 <Link color="inherit" underline="none" href="/blog" style={{ marginRight: '16px' }}>Blog</Link>
                 <Link color="inherit" underline="none" href="/new" style={{ marginRight: '16px' }}>New Post</Link>
-                <Link color="inherit" underline="none" href="/login" style={{ marginRight: '16px' }}>Login</Link>
+                <Link color="inherit" underline="none" href="/auth" style={{ marginRight: '16px' }}>Login</Link>
             </Toolbar>
         </AppBar>
     );
